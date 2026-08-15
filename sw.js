207938263b66e3523e3d915cbfb6c26e69ae7565
yujun0219 <bebetokorea@gmail.com>
2026-08-15 16:10:48 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pace-coach-v4";
+const CACHE_NAME = "pace-coach-v5";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
